9bce7482ad498bff3c2d046cb5c086be45bdfb68
UsterkoweOko <146815215+UsterkoweOko@users.noreply.github.com>
2024-07-01 10:00:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jul 1, 2024 17:00:00").getTime();
+var countDownDate = new Date("Jul 1, 2024 18:00:00").getTime();
 
 var x = setInterval(function() {
   var Audio = document.getElementById("snd");
